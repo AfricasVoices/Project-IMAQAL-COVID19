529900d659a55320bea8d247ded86d4e3344be66
Isaack Mwenda <mwendaisaac@gmail.com>
2020-05-20 13:40:01 +0300

    add covid som (diagnostic) data

--- configuration/code_schemes.py
+++ configuration/code_schemes.py
@@ -10,7 +10,9 @@ def _open_scheme(filename):
 
 
 class CodeSchemes(object):
-    S01E01 = _open_scheme("s01e01.json")
+    DIAGNOSTIC_S01E01 = _open_scheme("diagnostic_s01e01.json")
+    DIAGNOSTIC_S01E02 = _open_scheme("diagnostic_s01e02.json")
+    IMAQAL_COVID19_S01E01 = _open_scheme("s01e01.json")
 
     AGE = _open_scheme("age.json")
     AGE_CATEGORY = _open_scheme("age_category.json")
