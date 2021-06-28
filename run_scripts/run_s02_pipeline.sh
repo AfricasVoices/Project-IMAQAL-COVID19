efbea4029d3d2ff882c02b72ffbe7bc7f4f15ad9
Isaack Mwenda <mwendaisaac@gmail.com>
2021-06-28 10:12:03 +0300

    reproduce sal version

--- run_scripts/run_s02_pipeline.sh
+++ run_scripts/run_s02_pipeline.sh
@@ -32,8 +32,8 @@ echo "Starting run with id '$RUN_ID'"
 
 #./2_fetch_raw_data.sh "$USER" "$AVF_BUCKET_CREDENTIALS_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
 
-#./3_generate_outputs.sh --profile-memory "$PERFORMANCE_LOGS_DIR/memory-$RUN_ID.profile" \
-    #"$USER" "$PIPELINE_RUN_MODE" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
+./3_generate_outputs.sh --profile-memory "$PERFORMANCE_LOGS_DIR/memory-$RUN_ID.profile" \
+    "$USER" "$PIPELINE_RUN_MODE" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
 
 #./4_s02_coda_add.sh "$CODA_PUSH_CREDENTIALS_PATH" "$CODA_TOOLS_ROOT" "$DATA_ROOT"
 
